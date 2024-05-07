0c2127a277bbeb32adc166f31096262179a21fe8
TheSpotNFT <thespotnft@gmail.com>
2024-05-06 21:48:44 -0700

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div className='text-gray-500 font-mono mb-3 pt-96'><p>&copy; {new Date().getFullYear()} Avax Trucks</p></div>
+    <div className='text-gray-500 font-mono mb-3 pt-96'><p>&copy; {new Date().getFullYear()} Avax Cooks</p></div>
   )
 }
 

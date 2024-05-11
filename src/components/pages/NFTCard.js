6336a71a1398d6c10967ca171f2aacc743ec168d
thejunglemarket <96501055+TheSpotNFT@users.noreply.github.com>
2024-05-11 11:02:46 -0700

    adjust

--- src/components/pages/NFTCard.js
+++ src/components/pages/NFTCard.js
@@ -242,6 +242,7 @@ const NFTCard = ({ token, account, showBookmarks }) => {
       <h2 className="font-bold text-lg mt-2 text-center pb-8">{`Contributor: ${contributor || "None"}`}</h2>
       {/* Row for Like button and count */}
       <div className="flex items-center justify-end mt-2 space-x-2 pb-4">
+      <div className="pr-2"><p className="text-gray-600 text-lg font-bold pl-4">{likes}</p></div>
         <button
           onClick={handleLike}
           className={`bg-avax-red hover:opacity-100 text-white px-2 py-1 rounded ${hasLiked ? 'bg-avax-red opacity-100' : 'opacity-50'}`}
@@ -250,7 +251,7 @@ const NFTCard = ({ token, account, showBookmarks }) => {
           <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"/>
         </svg>
         </button>
-        <p className="text-gray-600 text-lg font-bold pl-4">{likes}</p>
+        
         <div className="pl-4"><button onClick={handleBookmark} className={`bg-avax-red hover:opacity-100 text-white px-2 py-1 rounded ${hasBookmarked ? 'bg-avax-red opacity-100' : 'opacity-50'}`}>
         {/* Simple bookmark icon */}
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="25" height="25" fill="currentColor">
